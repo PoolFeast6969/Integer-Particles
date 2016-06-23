2f2bedae216e4bcbea668142432f95c59c6ce238
“Jack <Jack@Mac-Mini.local>
2016-06-23 15:40:29 +1000

    So i made it really simple
    I overestimated my python
    
    (…it might be a dick joke, who knows?)

--- Physics.py
+++ Physics.py
@@ -1,13 +1,13 @@
 from timeit import default_timer as current_time
 from multiprocessing import Process, Pool
-from operator import methodcaller
+from time import sleep
 
-import multiprocessing, logging, pdb
-mpl = multiprocessing.log_to_stderr()
-mpl.setLevel(logging.INFO)
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Particle:
-    """ Particle class represents an indivisable object on a 2D plane that is accelerated by forces """
+    """ An indivisable object on a 2D plane that is accelerated by forces """
 
     def __init__(self, X_position, Y_position, X_velocity=0 ,Y_velocity=0, mass=600):
         self.mass = mass
@@ -30,21 +30,24 @@ class Particle:
                 self.position[axis] = self.position[axis] + int( elapsed_time * self.velocity[axis] )
                 # Reset the timer
                 self.time_of_last_update[axis] = current_time()
-        print(self.position)
 
-class Simulation (Process):
-    """ Simulation class represents an group of particles that can interact with each other """
+class World (Process):
+    """ A group of particles that can interact with each other """
 
-    def __init__(self, plane, worker_amount=4):
+    def __init__(self, plane, worker_amount=None, update_rate=30):
         Process.__init__(self)
         self.plane = plane
         self.workers = Pool(processes=worker_amount)
-
-    def call(self, number=0):
-        Particle.update(self.plane[number])
+        self.update_interval = 1/update_rate
+        logger.info('Created World with %s updates per second', update_rate)
 
     def run(self):
+        previous_update = current_time()
         while True:
             # Update each particle
-            number_of_particles = len(self.plane)
-            self.workers.map(self.call(), [0,1])
+            for particle in self.plane:
+                particle.update()
+            # Sleep to maintain update rate
+            update_delay = previous_update + self.update_interval - current_time()
+            previous_update = current_time()
+            sleep(max(0, update_delay))
--- Testing Controller.py
+++ Testing Controller.py
@@ -1,16 +1,19 @@
 from multiprocessing import Process, Pipe
-from Physics import Simulation, Particle
+from Physics import World, Particle
 from Output import Terminal_Output, Graphical_Output
 from random import randint as random_integer
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # Make up some shit to simulate
-random_plane = [Particle(X_position=random_integer(10, 999), Y_position=count, X_velocity=random_integer(-10, 10), Y_velocity=random_integer(-10, 10)) for count in range(29)]
-print(random_plane)
+particle_amount = 2000
+random_plane = [Particle(X_position=random_integer(10, 999), Y_position=random_integer(10, 999), X_velocity=random_integer(-10, 10), Y_velocity=random_integer(-10, 10)) for count in range(particle_amount)]
+logger.info('Created plane with %s particles', particle_amount)
 
-
-Jacks_sweet_thread = Simulation(random_plane)
-input('Press enter to start')
+Jacks_sweet_thread = World(random_plane)
+input('Press enter to start/stop')
 Jacks_sweet_thread.start()
-# Waits for you to press enter
-input('Press enter to stop')
+input()
 Jacks_sweet_thread.terminate()
