ec107c73d4289b199ae3ad8d4f5a3875c70c77a8
Jack Williams <hunter2@national,shitposting,agency>
2016-11-10 21:29:59 +1100

    It's goin' great i've fixed some of the problems that where inherent in the previous version already Like the particle traces and max no. of particles around 1000

--- Main.py
+++ Main.py
@@ -1,11 +1,21 @@
 # Create particles in shared memory
-number_of_particles = 10
+number_of_particles = 100
 width = 1000
 height = 1000
 from multiprocessing import RawArray
 position = {'x':RawArray('i', number_of_particles),'y':RawArray('i', number_of_particles)}
 velocity = {'x':RawArray('f', number_of_particles),'y':RawArray('f', number_of_particles)}
 time_since_update = {'x':RawArray('f', number_of_particles),'y':RawArray('f', number_of_particles)}
+acceleration = {'x':RawArray('f', number_of_particles),'y':RawArray('f', number_of_particles)}
+# Set acceleration
+for particle in range(len(acceleration['y'])):
+    acceleration['y'][particle] = 9.81
+for particle in range(len(acceleration['x'])):
+    acceleration['x'][particle] = 0
+# Randomise sideways velocity
+from random import randint as random_integer
+for particle in range(len(velocity['x'])):
+    velocity['x'][particle] = random_integer(-1000,1000)
 
 # Initialise physics threads
 frame_queue = {}
@@ -14,42 +24,44 @@ from Physics import Physics_Thread
 from multiprocessing import Queue, cpu_count
 for cpu_core in range(cpu_count()):
     frame_queue[cpu_core] = Queue()
-    phyics_process[cpu_core] = Physics_Thread(frame_queue[cpu_core], position, velocity, time_since_update)
+    phyics_process[cpu_core] = Physics_Thread(frame_queue[cpu_core], position, velocity, time_since_update, acceleration)
+
+# Initialise display
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((width, height))
 
 # Start physics threads
 for process in phyics_process.values():
     process.start()
 
-# Start displayed
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-
-# Main loop
-import numpy as np
+# Start the physics loops
+from numpy import empty, copy
 from timeit import default_timer as current_time
 from time import sleep
 update_interval = 1/60
 running = True
-blank_array = np.empty((width,height))
+blank_array = empty((width,height))
+# Update the time since update to just before it starts
+for axis in time_since_update:
+    for particle in range(len(time_since_update[axis])):
+        time_since_update[axis][particle] = current_time()
 previous_update = current_time()
+# Main loop
 while running:
+    # Listen for exit button
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # Request new frame
-    for frame_que in frame_queue.values():
-        frame_que.put(list(range(10)))
-
+    for cpu_core in range(cpu_count()):
+        frame_queue[cpu_core].put(list(range(round(number_of_particles/cpu_count()*(cpu_core)), round(number_of_particles/cpu_count()*(cpu_core+1)))))
     # Add to pygame array
-    pixel_array = blank_array
+    pixel_array = copy(blank_array)
     pixel_array[position['x'], position['y']] = 6000000
-
     # Update display
     pygame.surfarray.blit_array(screen, pixel_array)
     pygame.display.flip()
-
     # Sleep to maintain update rate
     update_delay = update_interval - (current_time() - previous_update)
     previous_update = current_time()
--- Physics.py
+++ Physics.py
@@ -5,49 +5,48 @@ from time import sleep
 class Physics_Thread (Process):
     """ A group of particles"""
 
-    def __init__(self, frame_queue, position, velocity, time_since_update):
+    def __init__(self, frame_queue, position, velocity, time_since_update, acceleration):
         Process.__init__(self)
         self.exit = Event()
         self.frame = frame_queue
         self.position = position
         self.velocity = velocity
         self.time_since_update = time_since_update
+        self.acceleration = acceleration
         print(self.name+' Initialised')
 
     def run(self):
-        for axis in self.time_since_update:
-            for particle in self.time_since_update[axis]:
-                particle = current_time()
-        print(self.time_since_update['x'][:])
         while not self.exit.is_set():
-            particles_to_update = self.frame.get()
+            particles_to_update = self.frame.get(timeout = 3)
             for particle in particles_to_update:
-                        for axis in self.position:
-                            # Calculate time since particle was last updated
-                            elapsed_time = current_time() - self.time_since_update[axis][particle]
-                            # Determine if a position change is needed
-                            try:
-                                inverse_velocity = 1 / (self.velocity[axis][particle] + 9.81 * elapsed_time)
-                            except ZeroDivisionError:
-                                inverse_velocity = 0
-                            if (elapsed_time >= abs(inverse_velocity)):
-                                # Calculate velocity
-                                self.velocity[axis][particle] = self.velocity[axis][particle] + 9.81 * elapsed_time
-                                # Edge bouncing
-                                if not (0 <= self.position[axis][particle] <= 1000):
-                                    self.velocity[axis][particle] = -self.velocity[axis][particle]/1.1
-                                    # Teleport back inside displayed range
-                                    if (self.position[axis][particle] > 500):
-                                        self.position[axis][particle] = 1000
-                                    else:
-                                        self.position[axis][particle] = 0
-                                # Move to new position
-                                self.position[axis][particle] = self.position[axis][particle] + int(elapsed_time * self.velocity[axis][particle])
-                                # Calculate position accuracy lost due to rounding
-                                lost_position = elapsed_time * self.velocity[axis][particle] - int(elapsed_time * self.velocity[axis][particle])
-                                # Reset the timer and adjust time for loss of position
-                                self.time_since_update[axis][particle] = current_time() - lost_position / self.velocity[axis][particle]
-
+                for axis in self.position:
+                    # Calculate time since particle was last updated
+                    elapsed_time = current_time() - self.time_since_update[axis][particle]
+                    # Determine if a position change is needed
+                    try:
+                        inverse_velocity = 1 / (self.velocity[axis][particle] + self.acceleration[axis][particle] * elapsed_time)
+                    except ZeroDivisionError:
+                        inverse_velocity = 0
+                    if (elapsed_time >= abs(inverse_velocity)):
+                        # Calculate velocity
+                        self.velocity[axis][particle] = self.velocity[axis][particle] + self.acceleration[axis][particle] * elapsed_time
+                        # Move to new position
+                        self.position[axis][particle] = self.position[axis][particle] + int(elapsed_time * self.velocity[axis][particle])
+                        # Edge bouncing
+                        if not (0 <= self.position[axis][particle] <= 999):
+                            self.velocity[axis][particle] = self.velocity[axis][particle]/-1.1
+                            # Teleport back inside displayed range
+                            if (self.position[axis][particle] > 500):
+                                self.position[axis][particle] = 999
+                            else:
+                                self.position[axis][particle] = 0
+                        # Calculate position accuracy lost due to rounding
+                        lost_position = elapsed_time * self.velocity[axis][particle] - int(elapsed_time * self.velocity[axis][particle])
+                        # Reset the timer and adjust time for loss of position
+                        try:
+                            self.time_since_update[axis][particle] = current_time() - lost_position / self.velocity[axis][particle]
+                        except ZeroDivisionError:
+                            self.time_since_update[axis][particle] = current_time()
     def terminate(self):
         print(self.name+' Exiting')
         self.exit.set()
