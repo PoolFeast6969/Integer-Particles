b202cd2502c524756daa3432e70c37345c47038c
Jack Williams <hunter2@national,shitposting,agency>
2017-06-08 20:33:19 +1000

    lmao

--- Physics.py
+++ Physics.py
@@ -45,7 +45,7 @@ class Physics_Thread (Process):
                         axis = self.particle_list[particle_index][axis_index]
                         if update_required:
                             # Calculate acceleration toward center
-                            #axis[self.properties.index('acceleration')] = (axis[self.properties.index('position')] - self.size[axis_index]/2) * -2
+                            axis[self.properties.index('acceleration')] = (axis[self.properties.index('position')] - self.size[axis_index]/2) * -2
                             # Get elapsed_time
                             elapsed_time = current_time() - axis[self.properties.index('time of update')]
                             # Calculate velocity
